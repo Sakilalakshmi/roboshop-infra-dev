6c95e791dc507eb47afb968ebeab27c337caab83
Sakilalakshmi <sakilalakshmi95@gmail.com>
2023-10-27 20:00:16 +0530

    openvpn install through script

--- 03-mongodb/mongodb.sh
+++ 03-mongodb/mongodb.sh
@@ -2,4 +2,4 @@
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/Sakilalakshmi/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+ansible-pull -U https://github.com/Sakilalakshmi/ansible-roles-sa.git -e component=mongodb main.yaml
